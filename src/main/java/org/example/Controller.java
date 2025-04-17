dc9b093908db4766d0eb5c694fe72ba8fa70759c
Ross Melo <rossmelo2005@gmail.com>
2025-04-17 12:54:09 +0100

    added handle message method to handle incoming message

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import org.example.Networking.TCPReceiver;
+import org.example.Networking.TCPSender;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Controller {
 
     private int cport;
@@ -13,23 +20,20 @@ public class Controller {
     /**
      * Creates a controller object
      * @param cport The port the controller in listening on
-     * @param R The replication Factor
-     * @param timeout The time to wait while waiting for a response in milliseconds
+     * @param R The replication Factor (The number of time each file is replicated).
+     * @param timeout The time to wait while waiting for an ACK in milliseconds
      * @param rebalancePeriod The time in seconds till the next rebalance operation
      */
-    public Controller(int cport, int R, int timeout, int rebalancePeriod) {
+    public Controller(int cport, int R, int timeout, int rebalancePeriod) throws IOException {
         this.cport = cport;
         this.replicationFactor = R;
         this.timeout = timeout;
         this.rebalancePeriod = rebalancePeriod;
-
         this.receiver = new TCPReceiver(cport);
-        this.sender = new TCPSender("localhost", cport);
-
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 4) {
             System.out.println("Usage: java Controller <cport> <R> <timeout> <rebalance_period>");
             return;
@@ -45,10 +49,68 @@ public class Controller {
 
     }
 
-    public void start() {
-
+    /**
+     * Starts the the reciever and does any necessary initialisation so the controller works properly
+     * @throws IOException
+     */
+    public void start() throws IOException {
         receiver.start();
-        sender.sendMessage("Hello, DStore!");
+
+    }
+
+    /**
+     * Handles any Incoming messages
+     * @param message The message recieved
+     * @param socket
+     * @throws IOException
+     */
+    public void handleMessage(String message , Socket socket) throws IOException {
+        switch (message) {
+            case "STORE": handleStore(); break;
+            case "LOAD": handleLoad(); break;
+            case "REMOVE": handleRemove(); break;
+            case "LIST": handleList(); break;
+            case "RELOAD": handleReload(); break;
+            case "JOIN": handleJoin(); break;
+            case "STORE_ACK": handleStoreAck(); break;
+            case "REMOVE_ACK": handleRemoveAck(); break;
+            case "ERROR_FILE_DOES_NOT_EXIST": handleRemoveAck(); break; // same effect
+            case "REBALANCE_COMPLETE": handleRebalanceComplete(); break;
+            default: System.err.println("Unknown command: " + message);
+        }
+    }
+
+    private void handleStore() {
+
+    }
+
+    private void handleLoad() {
+
+    }
+
+    private void handleRemove() {
+
+    }
+    private void handleList(String... args) {
+
+    }
+    private void handleReload() {
+
+    }
+
+    private void handleStoreAck() {
+
+    }
+
+    private void handleRemoveAck() {
+
+    }
+    private void handleRebalanceComplete() {
+
+    }
+
+    private void handleJoin() {
+
     }
 
 
